18c9e95a3022aff07a8fb2d1973032ebe6e66f04
Gouda <gouda.clark@gmail.com>
2018-09-07 09:17:44 -0400

    cleaned code up

--- main.js
+++ main.js
@@ -1,106 +1,49 @@
-console.log("hello world")
-
-
-
 $(() => {
-//     $(".scoreboard").append("<p>").append('User One').append("<p>").append("User Two")
-//     $("#categoryOnePointsTen").click(function() {
-//         console.log("10 point question")
-// })
-//     $("#categoryOnePointsTwenty").click(function() {
-//         console.log("20 point question")
-// }) 
-//     $("#categoryOnePointsThirty").click(function() {
-//         console.log("30 point question")
-// })
-//     $("#categoryOnePointsForty").click(function() {
-//         console.log("40 point question")
-// })
-//     $("#categoryOnePointsFifty").click(function() {
-//         console.log("50 point question")
-// })
 
-
-var score = 0
-//Add 10 points and display correct
+    var score = 0
+    //Add 10 points and display correct
     $(".correctTenPoints").on('click', () => {
-        score+=10
+        score += 10
         console.log(score)
-        swal("CORRECT!")
-       
-       
+        swal({
+            title: "CORRECT",
+        })
         newScore(score);
-        
-    
-     
     })
-//Add 20 points and display correct
-$(".correctTwentyPoints").on('click', () => {
-    score+=20
-    console.log(score)
-    swal("CORRECT!")
-
-    newScore(score)
-
-
-})
-
-//Add 30 points
-$(".correctThirtyPoints").on('click', () => {
-    score+=30
-    console.log(score)
-    swal("CORRECT!")
-
-    newScore(score);
-
-})
-
-//Alert for INCORRECT
+    //Add 20 points and display correct
+    $(".correctTwentyPoints").on('click', () => {
+        score += 20
+        console.log(score)
+        swal({
+            title: "CORRECT",
+        })
+        newScore(score)
+    })
+    //Add 30 points
+    $(".correctThirtyPoints").on('click', () => {
+        score += 30
+        console.log(score)
+        swal({
+            title: "CORRECT",
+        })
+        newScore(score);
+    })
+    //Alert for INCORRECT
     $(".incorrect").on('click', () => {
-        swal("INCORRECT!")
+        swal({
+            title: "INCORRECT",
+        })
         newScore(score);
-
     })
-    
     //Disable button once selected
     $(".btn-primary").on('click', (e) => {
         e.target.classList.add('disabled')
     })
-
-
-
-    })
-  
-
-
+})
 function newScore(score) {
     $(".scoreboard").text("Score: " + score);
-    if (score>=100)
-    swal({
-        title: "YOU WIN!",
-      });
-
-    }
-
-
-
-
-// $(".correctTenPoints").click(function(){
-//         console.log("add 10 points")
-// })
-// })
-
-
-   
-    
-
-
-
-
-// })
-// if loop for displaying correct/incorrect answer
-//Use JSON to display question in array; selections; 
-
-// //tenPointCategory
-// Question: ""
-// Answer: {'answer1' Points: 10, correct: true}
+    if (score >= 100)
+        swal({
+            title: "YOU WIN!",
+        });
+}
